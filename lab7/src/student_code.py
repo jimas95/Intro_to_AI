167bea42386022ed874d32b31226bda09711c6c4
jimas95 <jimas95@hotmail.com>
2021-05-27 21:26:09 -0500

    upload files

---
+++ lab7/src/student_code.py
@@ -0,0 +1,23 @@
+import common
+import math #note, for this lab only, your are allowed to import math
+
+def detect_slope_intercept(image):
+	# PUT YOUR CODE HERE
+	# access the image using "image[y][x]"
+	# where 0 <= y < common.constants.WIDTH and 0 <= x < common.constants.HEIGHT 
+	# set line.m and line.b
+	# to create an auxiliar bidimentional structure 
+	# you can use "space=common.init_space(heigh, width)"
+	line=common.Line()
+	line.m=0
+	line.b=0
+	return line
+
+def detect_circles(image):
+	# PUT YOUR CODE HERE
+	# access the image using "image[y][x]"
+	# where 0 <= y < common.constants.WIDTH and 0 <= x < common.constants.HEIGHT 
+	# to create an auxiliar bidimentional structure 
+	# you can use "space=common.init_space(heigh, width)"
+	return 0
+				
