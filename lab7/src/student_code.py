9fb6db9e086c1752f89d148371139ee1df63dc6d
jimas95 <jimas95@hotmail.com>
2021-05-30 03:49:50 -0500

    added code for circles

--- lab7/src/student_code.py
+++ lab7/src/student_code.py
@@ -32,7 +32,10 @@ def detect_circles(image):
 	# where 0 <= y < common.constants.WIDTH and 0 <= x < common.constants.HEIGHT 
 	# to create an auxiliar bidimentional structure 
 	# you can use "space=common.init_space(heigh, width)"
-	return 0
+	detector = CircleDetector(image)
+	detector.detect()
+	detected_cicles = detector.detected_cicles
+	return detected_cicles
 				
 
 
@@ -123,4 +126,77 @@ class LineDetector:
 
 
 
+
+# class for detecting lines
+class CircleDetector:
+
+
+	def __init__(self,image_):
+		self.gridSize = 200
+		self.radius = 30
+		self.detected_cicles = 0 
+		self.votes = common.init_space(self.gridSize,self.gridSize) # votes[a][b]
+		self.image = image_
+		self.points = []
+		self.size = len(image_)
+		
+		if(len(image_)!=len(image_[0])):
+			print("we have a problem!")
+		if(SIZE != self.size):
+			print("we have a problem!!")
+		
+
+	# scan the gray image and keep points of interest	
+	# we store only black pixels
+	def scanImage(self):
+		for y in range(self.size):
+			for x in range(self.size):
+				if(self.image[y][x]==0):
+					self.points.append((x,y))
+					# print(f"x,y {x},{y} --> {self.points[-1][0]},{self.points[-1][1]}")
+		print(f"scanning image --> found {len(self.points)} points")					
+		pass
+
+	# update the voting image 
+	# y = m*x + b
+	def createHoughSpace(self):
+		stepTotal = 360*2
+		dtheta = 2*math.pi/stepTotal
+		for point in self.points:
+			x = point[0]
+			y = point[1]
+			for theta in range(stepTotal):
+				a = x + self.radius*math.cos(theta*dtheta)
+				b = y - self.radius*math.sin(theta*dtheta)
+				self.vote(a,b)
+
+
+
+	def vote(self,a,b):
+		a = int(a+1)
+		b = int(b+1)
+		if(a>-1 and a<self.gridSize):
+			if(b>-1 and b<self.gridSize):
+				self.votes[a][b] = self.votes[a][b] + 1
+
+	def findCircles(self):
+		max_value = -1
+		for a in range(self.gridSize):
+			for b in range(self.gridSize):
+				if(self.votes[a][b]>max_value):
+					max_value = self.votes[a][b]
+		print(f"best vote --> {max_value}")
+
+		for a in range(self.gridSize):
+			for b in range(self.gridSize):
+				if(self.votes[a][b]>(max_value-50) and self.votes[a][b]>600):
+					# print(f"Circle --> votes: {self.votes[a][b]}")
+					self.detected_cicles = self.detected_cicles + 1
+	
+	# detect the line
+	def detect(self):
+		
+		self.scanImage()
+		self.createHoughSpace()
+		self.findCircles()
 
